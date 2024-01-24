ea068bf44ee8384a8729d2a58b3b00048475447c
blckvia <a.vyatkinn@icloud.com>
2024-01-24 09:13:44 +0300

    add task

--- ch4/structSlice.go
+++ ch4/structSlice.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	Name    string
+	Surname string
+}
+
+type names []Person
+
+func (a names) Len() int           { return len(a) }
+func (a names) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a names) Less(i, j int) bool { return a[i].Surname < a[j].Surname }
+
+func main() {
+	people := []Person{
+		{"Bob", "Smith"},
+		{"John", "Doe"},
+		{"Jane", "Doe"},
+	}
+
+	fmt.Println(people)
+	sort.Sort(names(people))
+	fmt.Println(people)
+}
