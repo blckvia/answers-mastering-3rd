43a6552ed8da33c663adac1777b51b0b3175a457
blckvia <a.vyatkinn@icloud.com>
2024-03-24 18:26:21 +0300

    add task3

---
+++ ch5/schema2.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) != 6 {
+		fmt.Println("Please provide: hostname port username password db")
+		return
+	}
+
+	host := arguments[1]
+	p := arguments[2]
+	user := arguments[3]
+	pass := arguments[4]
+	database := arguments[5]
+
+	// Port number SHOULD BE an integer
+	port, err := strconv.Atoi(p)
+	if err != nil {
+		fmt.Println("Not a valid port number:", err)
+		return
+	}
+
+	// connection string
+	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, pass, database)
+
+	// open PostgreSQL database
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		fmt.Println("Open():", err)
+		return
+	}
+	defer db.Close()
+
+	// Get all databases
+	rows, err := db.Query("SHOW DATABASES")
+	if err != nil {
+		fmt.Println("Query", err)
+		return
+	}
+
+	for rows.Next() {
+		var name string
+		err = rows.Scan(&name)
+		defer rows.Close()
+		if err != nil {
+			fmt.Println("Scan", err)
+			return
+		}
+		fmt.Println("*", name)
+	}
+
+	// Get all tables from __current__ database
+	query := "SHOW TABLES FROM " + database
+	rows, err = db.Query(query)
+	if err != nil {
+		fmt.Println("Query", err)
+		return
+	}
+
+	// This is how you process the rows that are returned from SELECT
+	for rows.Next() {
+		var name string
+		err = rows.Scan(&name)
+		if err != nil {
+			fmt.Println("Scan", err)
+			return
+		}
+		fmt.Println("+T", name)
+	}
+	defer rows.Close()
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module answers-mastering-3rd
 go 1.21.2
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
